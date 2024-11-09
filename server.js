1c8724eba31a55d8a1874d6e508c9f36d1d184b2
rkzin <enjoeinaoresponsa7@gmail.com>
2024-11-09 13:53:54 -0300

    vercel states

--- server.js
+++ server.js
@@ -20,20 +20,7 @@ app.post('/salvar', (req, res) => {
 
     // Dados formatados para salvar no arquivo
     const dados = `Nome: ${nome}\nEndereço: ${endereco}\nRG: ${rg}\nTelefone: ${telefone}\nE-mail: ${email}\nCPF: ${cpf}\nData: ${data}\n\n`;
-
-    // Definindo o caminho do arquivo "dados.txt"
-    const filePath = path.join(__dirname, 'dados.txt');
-    console.log('Caminho do arquivo:', filePath); // Log para verificar o caminho do arquivo
-
-    // Salvando os dados no arquivo "dados.txt"
-    fs.appendFile(filePath, dados, (err) => {
-        if (err) {
-            console.error('Erro ao salvar o arquivo:', err);
-            return res.status(500).send('Erro ao salvar o arquivo');
-        }
-        console.log('Arquivo salvo com sucesso!');
-        res.send('Arquivo salvo com sucesso!');
-    });
+	
 });
 
 // Inicialização do servidor
